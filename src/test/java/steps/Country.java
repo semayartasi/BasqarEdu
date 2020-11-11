940ac03ee1a79c19462e921fdc8178ac2faf8bc8
Sema <semayartasi@hotmail.com>
2020-11-11 10:04:00 -0500

    Fixed time issue

--- src/test/java/steps/Country.java
+++ src/test/java/steps/Country.java
@@ -6,9 +6,11 @@ import cucumber.api.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import utils.BaseDriver;
 
 public class Country {
@@ -19,34 +21,42 @@ public class Country {
 
         WebDriverWait wait=new WebDriverWait(driver,10);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Setup']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Setup']")));
         driver.findElement(By.xpath("//span[text()='Setup']")).click();
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Parameters']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Parameters']")));
         driver.findElement(By.xpath("//span[text()='Parameters']")).click();
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Countries']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Countries']")));
         driver.findElement(By.xpath("//span[text()='Countries']")).click();
 
 
     }
 
     @When("^I create a Country$")
-    public void iCreateACountry() {
+    public void iCreateACountry() throws InterruptedException {
         WebDriverWait wait=new WebDriverWait(driver,10);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("svg[data-icon='plus']")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("svg[data-icon='plus']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("svg[data-icon='plus']")));
         driver.findElement(By.cssSelector("svg[data-icon='plus']")).click();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("[formcontrolname=\"name\"]> input")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[formcontrolname=\"name\"]> input")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[formcontrolname=\"name\"]> input")));
         driver.findElement(By.cssSelector("[formcontrolname=\"name\"]> input")).sendKeys("countries");
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("ms-save-button")));
-        driver.findElement(By.tagName("ms-save-button"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("ms-save-button")));
+        driver.findElement(By.tagName("ms-save-button")).click();
     }
 
     @Then("^country is successfully created$")
     public void countryIsSuccessfullyCreated() {
-        WebDriverWait wait=new WebDriverWait(driver,10);
-       // wait.until(ExpectedConditions.textToBePresentInElement(alertDialog,)
-
+        WebDriverWait wait=new WebDriverWait(driver,5);
+         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[role='alertdialog']")));
+        WebElement alertDialog=driver.findElement(By.cssSelector("div[role='alertdialog']"));
+        Assert.assertEquals(alertDialog.getText(),"Country successfully created");
+        
     }
 }
